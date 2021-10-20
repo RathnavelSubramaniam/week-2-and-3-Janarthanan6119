a203bd061e84c0aca15bc269388d8f1f50a77e0f
Janarthanan6119 <91175755+Janarthanan6119@users.noreply.github.com>
2021-10-20 10:23:36 +0530

    Add files via upload

---
+++ Week 3/Coding Activity/3.1/3.1basictypes.js
@@ -0,0 +1,19 @@
+var myFirstName = "anu";
+var numberOfCities = 5;
+var havingFun = true;
+console.log(typeof (myFirstName));
+console.log(typeof (numberOfCities));
+console.log(typeof (havingFun));
+
+//your code here
+///////////////////////////////////
+//check results in browser console
+console.log('myFirstName: ', myFirstName);
+console.log('numberOfCities: ', numberOfCities);
+console.log('havingFun: ', havingFun);
+ 
+// Do not edit code past this point
+if (typeof module !== 'undefined') {
+  module.exports = { myFirstName, numberOfCities, havingFun };
+}
+ 
